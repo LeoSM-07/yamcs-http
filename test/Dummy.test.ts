8d79469bfa63bf44cdc94550907309ab517d2a18
Léo Mindlin <hello@leomindlin.com>
2025-08-12 15:41:42 -0400

    Fix Tests

--- test/Dummy.test.ts
+++ test/Dummy.test.ts
@@ -1,28 +1,74 @@
+import { HttpClient, HttpClientResponse } from "@effect/platform"
+import type { HttpClientRequest } from "@effect/platform/HttpClientRequest"
 import { describe, expect, it } from "bun:test"
 import { Effect, Layer, Logger, LogLevel } from "effect"
 import { Yamcs } from "src/yamcs-http.js"
+import getMissionDatabase from "./snapshots/mdb/getMissionDatabase.json" with { type: "json" }
+import getParameter from "./snapshots/mdb/getParameter.json" with { type: "json" }
+import getParameters from "./snapshots/mdb/getParameters.json" with { type: "json" }
 
-const testLayer = Layer.mergeAll(
-  Yamcs.Default,
+const makeJSONResponse = (req: HttpClientRequest, json: any) =>
+  Effect.succeed(
+    HttpClientResponse.fromWeb(
+      req,
+      new Response(
+        JSON.stringify(json),
+        { status: 200, headers: { "Content-Type": "application/json" } }
+      )
+    )
+  )
+
+const myClient = HttpClient.make((req) => {
+  const url = req.url.replace("http://localhost:8090/yamcs/api", "")
+
+  switch (url) {
+    case "/mdb/gs_backend/parameters//FC433/FlightComputer/pl_battery_voltage":
+      return makeJSONResponse(req, getParameter)
+    case "/mdb/gs_backend/parameters":
+      return makeJSONResponse(req, getParameters)
+    case "/mdb":
+      return makeJSONResponse(req, getMissionDatabase)
+    default:
+      // Default response for any other URL
+      return Effect.succeed(
+        HttpClientResponse.fromWeb(
+          req,
+          new Response(
+            JSON.stringify({
+              message: `Unhandled URL: ${url}`
+            }),
+            { status: 404, headers: { "Content-Type": "application/json" } }
+          )
+        )
+      )
+  }
+})
+
+const TestClient = Layer.succeed(HttpClient.HttpClient, myClient)
+
+const TestLayer = Layer.mergeAll(
+  Yamcs.DefaultWithoutDependencies,
   Logger.minimumLogLevel(LogLevel.Warning),
   Logger.pretty
+).pipe(
+  Layer.provide(TestClient)
 )
 
-describe("Yamcs MDB", () => {
-  it("Decode MDB Instance", async () => {
+describe("MDB", () => {
+  it("getMissionDatabase", async () => {
     const program = Effect.gen(function*() {
       const yamcs = yield* Yamcs
       const result = yield* yamcs.MDB.getMissionDatabase({ path: { instance: "gs_backend" } })
       return result
     }).pipe(
-      Effect.provide(testLayer)
+      Effect.provide(TestLayer)
     )
 
     const result = await Effect.runPromise(program)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchObject(getMissionDatabase)
   })
 
-  it("Decode Parameters List", async () => {
+  it("Decode `getParameters`", async () => {
     const program = Effect.gen(function*() {
       const yamcs = yield* Yamcs
       const result = yield* yamcs.MDB.getParameters({
@@ -32,14 +78,14 @@ describe("Yamcs MDB", () => {
       })
       return result
     }).pipe(
-      Effect.provide(testLayer)
+      Effect.provide(TestLayer)
     )
 
     const result = await Effect.runPromise(program)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchObject(getParameters)
   })
 
-  it("Decode Single Parameter", async () => {
+  it("Decode `getParameter`", async () => {
     const program = Effect.gen(function*() {
       const yamcs = yield* Yamcs
       const result = yield* yamcs.MDB.getParameter({
@@ -50,10 +96,10 @@ describe("Yamcs MDB", () => {
       })
       return result
     }).pipe(
-      Effect.provide(testLayer)
+      Effect.provide(TestLayer)
     )
 
     const result = await Effect.runPromise(program)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchObject(getParameter)
   })
 })
